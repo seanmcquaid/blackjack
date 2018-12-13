d453ecd190b9e2b68814a8dc4143c33a92645cda
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-13 15:34:19 -0500

    finished hit and stand buttons

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ let dealerHand = [];
 $(".deal-button").click(()=>{
     // we need a deck!
     // we need to shuffle it!
+    theDeck = freshDeck.slice()
     shuffleDeck(theDeck);
     // we have a shuffled deck, now give the players their cards
     // get the first element off of the deck and put it in top card
@@ -27,7 +28,29 @@ $(".deal-button").click(()=>{
     placeCard("dealer", 1, dealerHand[0]);
     placeCard("player", 2, playerHand[1]);
     placeCard("dealer", 2, dealerHand[1]);
+    cardTotal(playerHand, "player");
+    cardTotal(dealerHand, "dealer");
+
 })
+
+$(".hit-button").click(()=>{
+    const topCard = theDeck.shift();
+    playerHand.push(topCard);
+    placeCard("player", playerHand.length, topCard);
+    cardTotal(playerHand, "player");
+})
+
+$(".stand-button").click(()=>{
+    let dealersTotal = cardTotal(dealerHand, "dealer");
+    while (dealersTotal < 17) {
+        const topCard = theDeck.shift();
+        dealerHand.push(topCard);
+        placeCard("dealer", dealerHand.length, topCard);
+        dealersTotal = cardTotal(dealerHand, "dealer");
+    }
+    checkWinner();
+})
+
 
 function createDeck(){
     // this is a local variable, no one will know about this but this function
@@ -49,8 +72,8 @@ function shuffleDeck(aDeckToBeShuffled){
     // loop a lot
     // when the loop is done, the array will be shuffled
     for(let i =0; i < 100000; i++){
-        let rand1 = Math.floor(Math.random() * 52);
-        let rand2 = Math.floor(Math.random() * 52);
+        let rand1 = Math.floor(Math.random() * aDeckToBeShuffled.length);
+        let rand2 = Math.floor(Math.random() * aDeckToBeShuffled.length);
         // we need to switch adeckToBeShuffled[rand1] with adeckToBeShuffled[rand2]
         // but we have to save the value of one of them so we can keep it for later
         let card1Defender = aDeckToBeShuffled[rand1];
@@ -59,10 +82,34 @@ function shuffleDeck(aDeckToBeShuffled){
     }
 }
 
-function placeCard(who,where,what){
-    // who = player/ dealer
+function placeCard(who, where, what){
+    // who = player/ dealer cards
     // where = option 1-6
     // what = 1h - 13h, 1s - 13s, 1d - 13d, 1c - 13c
     const classSelector = `.${who}-cards .card-${where}`;
     $(classSelector).html(`<img src="/cards/${what}.png"/>`);
+}
+
+
+function cardTotal(hand, who){
+    // purpose:
+    // 1. find out the number and return it to update the DOM with the right number for whoevers hand it is
+    // 2. update the dom
+    let handTotal = 0;
+    hand.forEach((card, i)=>{
+        // console.log(card);
+        // copy everything in the string except for the last character
+        let thisCardsValue = card.slice(0,-1);
+        if(thisCardsValue > 10){
+            thisCardsValue = 10
+        }
+        handTotal += Number(thisCardsValue);
+    })
+    const classSelector = `.${who}-total`;
+    $(classSelector).html(handTotal);
+    return handTotal
+}
+
+function checkWinner(){
+
 }
